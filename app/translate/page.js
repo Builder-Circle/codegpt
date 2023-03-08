5fb32a708902ab378c6e08bd2dbaada37624a842
Paxwell <pompom231245@gmail.com>
2023-03-08 16:24:11 +0700

    update-search

--- app/translate/page.js
+++ app/translate/page.js
@@ -8,13 +8,22 @@ import {
     EditablePreview,
 } from "@chakra-ui/react";
 import { Textarea } from "@chakra-ui/react";
-import { useState } from 'react';
+import { useState,useRef } from 'react';
 import { Box, Button, Flex, Spacer, Text } from "@chakra-ui/react";
 import SelectLanguage from "@/component/selectLanguage";
 import Style from "@/app/translate/translate.module.css";
+import { Input } from "@chakra-ui/react";
+
+const baselanguage = [ "C", "C++", "C#", "Java", "Python","Javascript","Typescript","Go"];
+
 export default function translate() {
     const [file, setfile] = useState(null);
     const [strfile,setStrfile] = useState("");
+    const [language, setLanguage] = useState("detect language");
+    const [language2, setLanguage2] = useState("");
+    const converButton = useRef(null);
+    const [open, setOpen] = useState(false);
+    const [openSuggestions, setOpenSuggestions] = useState(false);
     function handleFileChange(e) {
         const tmpfile = e.target.files[0];
         if(!tmpfile) {
@@ -39,6 +48,15 @@ export default function translate() {
             })
             .catch((err) => console.error(err));
     }
+    function clickconvert() {
+        converButton.current.click();
+        setOpenSuggestions(false);
+    }
+    function convert() {
+
+        console.log("convert");
+    }
+
     return (
         <>
             <Flex
@@ -58,7 +76,7 @@ export default function translate() {
                     alignItems={"center"}
                 >
                     <Box mb={"1rem"}>
-                        <SelectLanguage />
+                        <Input color={"gray.400"} value={language} isReadOnly textAlign={"center"}  />
                     </Box>
                     <Textarea border={"0"} minW={"400px"} minH={"400px"} mb={"1rem"} value={strfile} placeholder="Take some code here ..." 
                     onChange={(e)=>(setStrfile(e.target.value))} >
@@ -74,8 +92,9 @@ export default function translate() {
                 </Box>
                 <Box>
                     <Flex>
-                        <Button colorScheme="teal" size="lg">
+                        <Button colorScheme="teal" size="lg" ref={converButton} onClick={convert}>
                             Convert
+                            
                         </Button>
                     </Flex>
                 </Box>
@@ -90,8 +109,34 @@ export default function translate() {
                     justifyContent={"space-evenly"}
                     alignItems={"center"}
                 >
-                    <Box mb={"1rem"}>
-                        <SelectLanguage />
+                    <Box width={"211px"} >
+                        <Box mb={"0.5rem"}>
+                            <form action="#clickconvert" onSubmit={(e)=>{
+                                e.preventDefault();
+                                clickconvert();
+                            }}>
+                                <Input color={"gray.400"} value={language2}  textAlign={"center"} onChange={(e)=>{
+                                    setLanguage2(e.target.value)
+                                    setOpenSuggestions(true);
+                                    if(e.target.value==="")
+                                        setOpenSuggestions(false);
+                                    setOpen(true);
+                                }} placeholder="Typescript"  />
+                            </form>
+                        </Box >
+                        <Box position={"relative"}>
+                            {openSuggestions&&<Box display={"flex"} flexDirection={"column"} border={"1px"} borderColor={"gray.400"} color={"gray.400"} position={"absolute"} zIndex={"1"} width={"100%"} >
+                                {language2&& open &&baselanguage.filter((item)=>(item.toLowerCase().includes(language2.toLowerCase()))).map((item)=>(
+                                    <Box key={item} onClick={()=>{
+                                        setLanguage2(item)
+                                        setOpen(false);
+                                    }} p={"0.5rem"} 
+                                    cursor={"pointer"} _hover={{backgroundColor:"gray.100"}}>{item}
+                                    </Box> 
+                                ))  }
+
+                            </Box>}
+                        </Box>
                     </Box>
 
                     <Textarea border={"0"} minW={"400px"} minH={"400px"} mb={"1rem"} placeholder="Code ..." isReadOnly  >
--- component/selectLanguage.js
+++ component/selectLanguage.js
@@ -1,44 +1,10 @@
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-  } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
-import { ChevronDownIcon } from '@chakra-ui/icons'
-import { Image } from '@chakra-ui/react'
-export default function selectLanguage() {
+import { Select, } from "@chakra-ui/react";
+export default function selectLanguage({language="Select Language",isdisable=false}) {
     return (
-        <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} >
-                Select Language
-            </MenuButton>
-            <MenuList>
-                <MenuItem minH="48px">
-                    <Image
-                        boxSize="2rem"
-                        borderRadius="full"
-                        src="https://placekitten.com/100/100"
-                        alt="Fluffybuns the destroyer"
-                        mr="12px"
-                    />
-                    <span>Fluffybuns the Destroyer</span>
-                </MenuItem>
-                <MenuItem minH="40px">
-                    <Image
-                        boxSize="2rem"
-                        borderRadius="full"
-                        src="https://placekitten.com/120/120"
-                        alt="Simon the pensive"
-                        mr="12px"
-                    />
-                    <span>Simon the pensive</span>
-                </MenuItem>
-            </MenuList>
-        </Menu>
+        <Select placeholder={language}  isDisabled={isdisable} >
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+        </Select>
     );
 }
