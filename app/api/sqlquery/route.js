db2cb37f66dc4e0cc46163b5bcfee8b2ed7768df
ItsZodiaX <59441461+ItsZodiaX@users.noreply.github.com>
2023-03-10 21:35:34 +0700

    sqlquery\route.js: Support multiple tables

--- app/api/sqlquery/route.js
+++ app/api/sqlquery/route.js
@@ -3,11 +3,15 @@ import openai from "../openai/openai";
 export async function POST(request) {
     try {
         const {dataRequire, alltable} = await request.json();
+        let database = ""
+        for (let i = 0; i < alltable.length; i++) {
+            database += `Table name is ${alltable[i].tableName}, primary key is ${alltable[i].primarykey}, foreign key is ${alltable[i].foreignkey} and attributes are ${alltable[i].attributes}\n`
+        }
         const chat = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [
                 {"role": "system", "content": "You are best programmer that ever exist in the world!"},
-                {"role": "user", "content": `What's the MySQL command for ${dataRequire} in the table which table name is ${alltable[0].tableName}, primary key is ${alltable[0].primarykey}, foreign key is ${alltable[0].foreignkey} and attributes are ${alltable[0].attributes}\n\n`}
+                {"role": "user", "content": `What's the MySQL command for ${dataRequire} which in following data ${database}`}
             ],
             temperature: 0,
         });
