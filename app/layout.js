135792680e3aacdef6c33ae5edd3ee992535e62f
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2023-03-10 23:06:12 +0700

    feat: adding GA

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,38 @@
-'use client'
-import Navbar from '@/component/Navbar'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider } from '@chakra-ui/react'
+"use client";
+import Navbar from "@/component/Navbar";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Script } from "";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content='CodeGPT' />
-        <meta name='author' content='CodeGPT' />
-        <meta name='keywords' content='CodeGPT' />
-        <meta name='theme-color' content='#000000' />
-        
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="CodeGPT" />
+        <meta name="author" content="CodeGPT" />
+        <meta name="keywords" content="CodeGPT" />
+        <meta name="theme-color" content="#000000" />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+        />
+          
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_TRACKING}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
         <title>CodeGPT</title>
       </head>
       <body>
@@ -26,5 +44,5 @@ export default function RootLayout({ children }) {
         </CacheProvider>
       </body>
     </html>
-  )
+  );
 }
